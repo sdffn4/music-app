c45a47fa138e6ee5047ac3851fd930bf0d2c9157
zhopojzhuj <zhopojzhuj@proton.me>
2023-01-23 21:24:17 +0200

    remove swr & install react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,28 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/Layout";
+import { useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <SessionProvider session={session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
