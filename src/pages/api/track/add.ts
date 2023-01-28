f238b6897fc602ecb9e36152735e2633821d2591
zhopojzhuj <zhopojzhuj@proton.me>
2023-01-28 08:49:17 +0200

    create api endpoints to add/remove tracks

---
+++ src/pages/api/track/add.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../lib/prismadb";
+
+interface Request extends NextApiRequest {
+  body: {
+    playlistId: string;
+    trackId: string;
+  };
+}
+
+export default async function handler(req: Request, res: NextApiResponse) {
+  if (req.method !== "POST") return res.status(405);
+
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) return res.status(403);
+
+  const { playlistId, trackId } = req.body;
+
+  try {
+    const playlist = await prisma.playlist.findUnique({
+      where: {
+        id: playlistId,
+      },
+      select: {
+        user: {
+          select: {
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (playlist && playlist.user.email !== session.user?.email) {
+      return res.status(403);
+    }
+
+    await prisma.tracksOnPlaylists.create({
+      data: {
+        playlistId,
+        trackId,
+      },
+    });
+
+    return res.status(200).json({ status: true, trackId, playlistId });
+  } catch (error) {
+    return res.status(500);
+  }
+}
