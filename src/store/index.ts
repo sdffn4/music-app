a3b94b52f8008b3e104cbaced0e3cd933a24d444
zhopojzhuj <zhopojzhuj@proton.me>
2023-01-28 23:46:25 +0200

    extend store to manage queue

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,80 @@
 import { TrackType } from "@/types";
+import { nanoid } from "nanoid";
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type QueueInstance = {
+  id: string;
+  track: TrackType;
+};
+
+type Queue = {
+  index: number;
+  instances: QueueInstance[];
+};
 
 interface Store {
-  currentTrack: TrackType | null;
-  setCurrentTrack: (track: TrackType) => void;
-
   isPlaying: boolean;
   setIsPlaying: (value: boolean) => void;
+
+  queue: Queue;
+  setQueueInstance: (index: number) => void;
+  setQueue: (index: number, tracks: TrackType[]) => void;
+
+  skipBackward: () => void;
+  skipForward: () => void;
 }
 
-const usePlayerStore = create<Store>((set) => ({
-  currentTrack: null,
-  setCurrentTrack: (track: TrackType) =>
-    set((state) => ({ currentTrack: track })),
-  isPlaying: false,
-  setIsPlaying: (value: boolean) => set((state) => ({ isPlaying: value })),
-}));
+const usePlayerStore = create<Store, [["zustand/devtools", Store]]>(
+  devtools((set) => ({
+    isPlaying: false,
+    setIsPlaying: (value: boolean) => set(() => ({ isPlaying: value })),
+
+    queue: {
+      index: -1,
+      instances: [],
+    },
+
+    setQueue: (index: number, tracks: TrackType[]) =>
+      set(() => ({
+        queue: {
+          index,
+          instances: tracks.map((track) => ({ id: nanoid(), track })),
+        },
+      })),
+
+    setQueueInstance: (index: number) =>
+      set((state) => ({
+        queue: {
+          ...state.queue,
+          index,
+        },
+      })),
+
+    skipBackward: () =>
+      set((state) => {
+        if (state.queue.index !== -1) {
+          return {
+            queue: {
+              ...state.queue,
+              index: state.queue.index - 1,
+            },
+          };
+        } else return state;
+      }),
+
+    skipForward: () =>
+      set((state) => {
+        if (state.queue.index !== -1) {
+          return {
+            queue: {
+              ...state.queue,
+              index: state.queue.index + 1,
+            },
+          };
+        } else return state;
+      }),
+  }))
+);
 
 export default usePlayerStore;
