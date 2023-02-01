7fa537b67eca93aeea26de6c44745f1b30379865
zhopojzhuj <zhopojzhuj@proton.me>
2023-02-01 09:02:54 +0200

    create function to remove track from queue

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { TrackType } from "@/types";
 import { nanoid } from "nanoid";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
 
 type QueueInstance = {
   id: string;
@@ -10,6 +9,7 @@ type QueueInstance = {
 
 type Queue = {
   index: number;
+
   instances: QueueInstance[];
 };
 
@@ -22,80 +22,102 @@ interface Store {
   setQueue: (index: number, tracks: TrackType[]) => void;
 
   addToQueue: (track: TrackType) => void;
+  removeFromQueue: (index: number) => void;
 
   skipBackward: () => void;
   skipForward: () => void;
 }
 
-const usePlayerStore = create<Store, [["zustand/devtools", Store]]>(
-  devtools((set) => ({
-    isPlaying: false,
-    setIsPlaying: (value: boolean) => set(() => ({ isPlaying: value })),
+const usePlayerStore = create<Store>((set) => ({
+  isPlaying: false,
+  setIsPlaying: (value: boolean) => set(() => ({ isPlaying: value })),
 
-    queue: {
-      index: -1,
-      instances: [],
-    },
+  queue: {
+    index: -1,
+    instances: [],
+  },
 
-    setQueue: (index: number, tracks: TrackType[]) =>
-      set(() => ({
+  setQueue: (index: number, tracks: TrackType[]) =>
+    set(() => ({
+      queue: {
+        index,
+        instances: tracks.map((track) => ({ id: nanoid(), track })),
+      },
+    })),
+
+  setQueueInstance: (index: number) =>
+    set((state) => ({
+      queue: {
+        ...state.queue,
+        index,
+      },
+    })),
+
+  addToQueue: (track: TrackType) =>
+    set((state) => {
+      if (state.queue.instances.length === 0) {
+        return {
+          queue: {
+            index: 0,
+            instances: [{ id: nanoid(), track }],
+          },
+        };
+      } else {
+        return {
+          queue: {
+            ...state.queue,
+            instances: [...state.queue.instances, { id: nanoid(), track }],
+          },
+        };
+      }
+    }),
+
+  removeFromQueue: (index: number) =>
+    set((state) => {
+      const newIndex: number =
+        state.queue.index === index &&
+        state.queue.instances.length - 1 === index
+          ? state.queue.index - 1
+          : state.queue.index > index
+          ? state.queue.index - 1
+          : state.queue.index;
+
+      const newInstances = state.queue.instances.filter(
+        (_, idx) => index !== idx
+      );
+
+      return {
         queue: {
-          index,
-          instances: tracks.map((track) => ({ id: nanoid(), track })),
+          index: newIndex,
+          instances: newInstances,
         },
-      })),
+        isPlaying: newInstances.length === 0 ? false : state.isPlaying,
+      };
+    }),
 
-    setQueueInstance: (index: number) =>
-      set((state) => ({
-        queue: {
-          ...state.queue,
-          index,
-        },
-      })),
+  skipBackward: () =>
+    set((state) => {
+      if (state.queue.index !== -1) {
+        return {
+          queue: {
+            ...state.queue,
+            index: state.queue.index - 1,
+          },
+        };
+      } else return state;
+    }),
 
-    addToQueue: (track: TrackType) =>
-      set((state) => {
-        if (state.queue.instances.length === 0) {
-          return {
-            queue: {
-              index: 0,
-              instances: [{ id: nanoid(), track }],
-            },
-          };
-        } else {
-          return {
-            queue: {
-              ...state.queue,
-              instances: [...state.queue.instances, { id: nanoid(), track }],
-            },
-          };
-        }
-      }),
-
-    skipBackward: () =>
-      set((state) => {
-        if (state.queue.index !== -1) {
-          return {
-            queue: {
-              ...state.queue,
-              index: state.queue.index - 1,
-            },
-          };
-        } else return state;
-      }),
-
-    skipForward: () =>
-      set((state) => {
-        if (state.queue.index !== -1) {
-          return {
-            queue: {
-              ...state.queue,
-              index: state.queue.index + 1,
-            },
-          };
-        } else return state;
-      }),
-  }))
-);
+  skipForward: () =>
+    set((state) => {
+      if (state.queue.index !== -1) {
+        return {
+          queue: {
+            ...state.queue,
+            index: state.queue.index + 1,
+          },
+        };
+      } else return state;
+    }),
+}));
 
 export default usePlayerStore;
