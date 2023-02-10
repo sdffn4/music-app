361f4f48ab28eed5a03bcbf8067a833733d3935b
zhopojzhuj <zhopojzhuj@proton.me>
2023-02-10 14:33:36 +0200

    create api endpoints to sub/unsub

---
+++ src/pages/api/playlist/subscribe.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") return res.status(405);
+
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) return res.status(403);
+
+  try {
+    const { playlistId } = req.body;
+
+    const resp = await prisma.subscription.create({
+      data: {
+        playlist: {
+          connect: {
+            id: playlistId,
+          },
+        },
+        user: {
+          connect: {
+            email: session.user?.email as string | undefined,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({ subscriptionId: resp.id });
+  } catch (error) {
+    return res.status(500);
+  }
+}
