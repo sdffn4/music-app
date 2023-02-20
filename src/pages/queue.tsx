cecffef700d4c4cf7ae59782f4e2ba37eaabea7f
sdffn4 <sdffn2@gmail.com>
2023-02-20 12:22:38 +0200

    refactor: move queue.tsx back to /pages & make it more readable

---
+++ src/pages/queue.tsx
@@ -0,0 +1,51 @@
+import Track from "@/components/Track";
+import usePlayerStore from "@/store";
+
+export default function Queue() {
+  const { isPlaying, setIsPlaying, queue, setQueueInstance } = usePlayerStore(
+    (state) => state
+  );
+
+  const play = (index: number) => {
+    setQueueInstance(index);
+
+    const clickedOnCurrentPlayingTrack = queue.index === index && isPlaying;
+    const clickedOnCurrentNotPlayingTrack = queue.index === index && !isPlaying;
+    const clickedOnNotCurrentTrack = queue.index !== index;
+
+    if (clickedOnCurrentPlayingTrack) {
+      setIsPlaying(false);
+    }
+
+    if (clickedOnCurrentNotPlayingTrack || clickedOnNotCurrentTrack) {
+      setIsPlaying(true);
+    }
+  };
+
+  if (queue.instances.length === 0) {
+    return (
+      <div className="flex flex-col h-full justify-center items-center">
+        Queue is empty
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-4">
+      {queue.instances.map((instance, index) => {
+        const currentInstance = queue.instances[queue.index];
+        const isActive = instance.id === currentInstance.id && isPlaying;
+
+        return (
+          <Track
+            key={instance.id}
+            index={index + 1}
+            isActive={isActive}
+            track={instance.track}
+            onClick={() => play(index)}
+          />
+        );
+      })}
+    </div>
+  );
+}
