b455e75514c5c05e61d09909b2731395ffc7e7d1
sdffn4 <sdffn2@gmail.com>
2023-02-23 17:28:46 +0200

    edit fetcher to upload playlist cover

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -2,14 +2,47 @@ import axios from "axios";
 
 import type { LibraryApi } from "@/pages/api/library";
 import type { CreatePlaylistApi } from "@/pages/api/playlist/create";
+import { AuthorizeUploadApi } from "@/pages/api/authorize_upload";
+import { UploadApiResponse } from "cloudinary";
 
 export const fetchLibrary = async () => {
   return (await axios.get<LibraryApi>(`/api/library`)).data;
 };
 
-export const createPlaylist = async (args: { id: string; title: string }) => {
-  return (await axios.post<CreatePlaylistApi>(`/api/playlist/create`, args))
-    .data;
+const uploadImage = async (file: File) => {
+  const signData = (
+    await axios.post<AuthorizeUploadApi>(`/api/authorize_upload`, {
+      folder: "covers",
+    })
+  ).data;
+
+  const url = `https://api.cloudinary.com/v1_1/${signData.cloud_name}/image/upload`;
+
+  const formData = new FormData();
+
+  formData.append("file", file);
+  formData.append("api_key", signData.api_key);
+  formData.append("timestamp", signData.timestamp);
+  formData.append("signature", signData.signature);
+  formData.append("folder", "covers");
+
+  return (await axios.post<UploadApiResponse>(url, formData)).data.secure_url;
+};
+
+export const createPlaylist = async (args: {
+  id: string;
+  title: string;
+  file: File | undefined;
+}) => {
+  const secure_url = args.file ? await uploadImage(args.file) : null;
+
+  return (
+    await axios.post<CreatePlaylistApi>(`/api/playlist/create`, {
+      id: args.id,
+      title: args.title,
+      cover: secure_url,
+    })
+  ).data;
 };
 
 export const removePlaylist = async (args: { playlistId: string }) => {
