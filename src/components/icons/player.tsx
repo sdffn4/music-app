0442252d37aea29314ece2161196fcd64ca229c8
sdffn4 <sdffn2@gmail.com>
2023-03-01 14:23:59 +0200

    add icons for the volume button

--- src/components/icons/player.tsx
+++ src/components/icons/player.tsx
@@ -5,21 +5,41 @@ import {
   RiPlayFill,
   RiShuffleLine,
   RiPlayListAddLine,
+  RiVolumeMuteLine,
+  RiVolumeDownLine,
+  RiVolumeUpLine,
 } from "react-icons/ri";
 
 const playerIcon = `w-5 h-5`;
 
 export const PlayIcon = () => <RiPlayFill className={`${playerIcon}`} />;
+
 export const PauseIcon = () => <RiPauseFill className={`${playerIcon}`} />;
+
 export const SkipBackwardIcon = () => (
   <RiRewindFill className={`${playerIcon} opacity-50`} />
 );
+
 export const SkipForwardIcon = () => (
   <RiSpeedFill className={`${playerIcon} opacity-50`} />
 );
+
 export const ShuffleIconActive = () => (
   <RiShuffleLine className={`${playerIcon}`} />
 );
+
 export const CollectIconAction = () => (
   <RiPlayListAddLine className={`${playerIcon}`} />
+);
+
+export const VolumeMuteIcon = () => (
+  <RiVolumeMuteLine className={`${playerIcon} opacity-70`} />
+);
+
+export const VolumeDownIcon = () => (
+  <RiVolumeDownLine className={`${playerIcon} opacity-70`} />
+);
+
+export const VolumeUpIcon = () => (
+  <RiVolumeUpLine className={`${playerIcon} opacity-70`} />
 );
